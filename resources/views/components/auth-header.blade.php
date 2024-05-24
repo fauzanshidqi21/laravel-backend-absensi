1bed21f0342788ca6de3261764ece13f3b2c2550
Fauzan Shidqi <fauzanshidqi21@gmail.com>
2024-05-24 23:34:53 +0700

    edit,delete,update

--- resources/views/components/auth-header.blade.php
+++ resources/views/components/auth-header.blade.php
@@ -1,3 +1,3 @@
 <div class="login-brand">
-    <img src="{{ asset('img/stisla-fill.svg') }}" alt="logo" width="100" class="shadow-light rounded-circle">
+    <img src="{{ asset('img/hsn.svg') }}" alt="logo" width="100" class="shadow-light rounded-circle">
 </div>
